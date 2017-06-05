e1fec80181078de87fcf09e0aadcacedda8818f2
dmayes403 <davidrmayes@gmail.com>
2017-06-04 20:58:12 -0600

    learning about the 8 different life cycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit , OnChanges, DoCheck, AfterContentInit, AfterContentChecked {
   @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+  @Input() name: string;
+
+  constructor() {
+    console.log('constructor called')
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called')
+    console.log(changes)
+  }
 
   ngOnInit() {
+    console.log('ngOninit called')
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
   }
 
 }
